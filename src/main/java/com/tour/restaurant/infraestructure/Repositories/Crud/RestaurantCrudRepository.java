9586a5007d3b59d7aba7d9e50a3e1bda48bcf57e
AT1927 <atmunoz1927@gmail.com>
2024-05-07 09:46:15 -0500

    Change RestaurantCrudRepository

--- src/main/java/com/tour/restaurant/infraestructure/Repositories/Crud/RestaurantCrudRepository.java
+++ src/main/java/com/tour/restaurant/infraestructure/Repositories/Crud/RestaurantCrudRepository.java
@@ -1,9 +1,7 @@
 package com.tour.restaurant.infraestructure.Repositories.Crud;
-
+import com.tour.restaurant.infraestructure.Entities.*;
 import com.tour.restaurant.infraestructure.Entities.Restaurant;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface RestaurantCrudRepository extends CrudRepository<Restaurant, Long> {
 }
