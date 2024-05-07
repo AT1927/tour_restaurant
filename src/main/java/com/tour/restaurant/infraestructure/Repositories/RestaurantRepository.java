92b6743b45489697e044390bfda30a2d0ea00c9b
AT1927 <atmunoz1927@gmail.com>
2024-05-07 09:37:38 -0500

    Change RestaurantRepository

--- src/main/java/com/tour/restaurant/infraestructure/Repositories/RestaurantRepository.java
+++ src/main/java/com/tour/restaurant/infraestructure/Repositories/RestaurantRepository.java
@@ -10,30 +10,30 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 @Repository
-public abstract class RestauranteRepository implements RestaurantRepositoryDomain {
+public abstract class RestaurantRepository implements RestaurantRepositoryDomain {
     @Autowired
-    public RestaurantCrudRepository restauranteRepo;
+    public RestaurantCrudRepository restaurantRepo;
     @Autowired
     public RestaurantMapper mapper;
 
     @Override
     public List<RestaurantDTO> getAll(){
-        List<Restaurant> restaurantes = (List<Restaurant>) restauranteRepo.findAll();
-        return mapper.toRestaurants(restaurantes);
+        List<Restaurant> restaurants = (List<Restaurant>) restaurantRepo.findAll();
+        return mapper.toRestaurants(restaurants);
     }
    @Override
     public Optional<Restaurant> getById(long idRestaurant){
-        Optional<Restaurant> restaurant = restauranteRepo.findById(idRestaurant);
+        Optional<Restaurant> restaurant = restaurantRepo.findById(idRestaurant);
         return mapper.toRestaurantsOptional(restaurant);
     }
 
     @Override
     public RestaurantDTO save(RestaurantDTO restaurantDTO){
-        Restaurant restaurante = mapper.toRestaurant(restaurantDTO);
-        return mapper.toRestaurantDTO(restauranteRepo.save(restaurante));
+        Restaurant restaurant = mapper.toRestaurant(restaurantDTO);
+        return mapper.toRestaurantDTO(restaurantRepo.save(restaurant));
     }
     @Override
     public void deleteByID(long idRestaurant){
-        restauranteRepo.deleteById(idRestaurant);
+        restaurantRepo.deleteById(idRestaurant);
     }
 }
